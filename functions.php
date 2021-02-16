65df5c5cafe43219cf30e3b514807c527b88ef8f
Ty Eggen <tycho.eggen@gmail.com>
2021-02-16 13:36:32 -0800

    Added Stripe logo and button formatting

--- functions.php
+++ functions.php
@@ -9,12 +9,26 @@ function s3b_button($atts)
             'price' => '75',
             'period' => 'month',
             'price_id' => '',
-            'quantity' => 1
+            'quantity' => 1,
+            'currency_prefix' => '$'
         ),
         $atts
     );
 
-    return sprintf("<button class=\"s3b-button s3b-button-custom\" data-price-id=\"%s\" data-quantity=\"%d\" data-mode=\"%s\">CHECKOUT with Stripe: %d per %s</button>", $atts['price_id'], $atts['quantity'], $atts['mode'], number_format($atts['price'], 2), $atts['period']);
+    $button_elements = array();
+    $button_elements[] = "<button class=\"s3b-button s3b-button-custom\" data-price-id=\"%s\" data-quantity=\"%d\" data-mode=\"%s\">";
+    $button_elements[] = "<img class=\"s3b-logo\" src=\"" . plugin_dir_url(__FILE__)."assets/Stripe-wordmark-white.svg" . "\" /><br />";
+    $button_elements[] = "%s%d per %s";
+    $button_elements[] = "</button>";
+    
+    $button_args = array();
+    $button_args[] = $atts['price_id'];
+    $button_args[] = $atts['quantity'];
+    $button_args[] = $atts['mode'];
+    $button_args[] = $atts['currency_prefix'];
+    $button_args[] = number_format($atts['price'], 2);
+    $button_args[] = $atts['period'];
+    return sprintf(join(" ", $button_elements), ...$button_args);
 }
 
 function s3b_create_checkout_session($body)
