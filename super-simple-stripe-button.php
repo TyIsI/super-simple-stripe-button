83a818aff687b0b8b44de6ad5fb7380196a41b31
Ty Eggen <tycho.eggen@gmail.com>
2025-01-03 22:29:41 +0000

    chore: bump versions

--- super-simple-stripe-button.php
+++ super-simple-stripe-button.php
@@ -13,7 +13,7 @@
  * Plugin Name:       Super Simple Stripe Button
  * Plugin URI:        https://github.com/tyisi/super-simple-stripe-button/
  * Description:       A super simple Stripe button plugin
- * Version:           2.0.2
+ * Version:           2.0.3
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Ty Eggen
